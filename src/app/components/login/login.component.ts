e83ee0d911c08bef70cfdeca437d79cd5d8b0bd8
sagar <sagarali.cse21@gmail.com>
2024-05-14 20:02:37 +0600

    modified interceptor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -70,6 +70,8 @@ export class LoginComponent implements OnInit {
             });
     
             this.router.navigate(['dashboard']);
+            this.router.navigateByUrl('/dashboard');
+
           //  window.location.href='/dashboard';
 
           }
